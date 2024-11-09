c1c99a4aa2cea599f10bef5263fdc601f8351d7e
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 20:20:19 -0300

    Added Swagger documentation to endpoint POST:movie-reviews

--- src/modules/movie-review/movie-review.controller.ts
+++ src/modules/movie-review/movie-review.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { CreateMovieReviewDto } from './dtos/create-movie-review.dto';
 import { MovieReviewService } from './movie-review.service';
@@ -9,6 +10,34 @@ export class MovieReviewController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: 'Create a movie review',
+    description: 'Create a movie review',
+  })
+  @ApiBody({
+    type: CreateMovieReviewDto,
+    examples: {
+      Inception: {
+        value: {
+          title: 'Inception',
+          notes: 'Great movie',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Movie review created',
+    example: {
+      movieReviewId: 1,
+      title: 'Inception',
+      releaseDate: '2010-07-16T00:00:00.000Z',
+      rating: 8.8,
+      directors: ['Christopher Nolan'],
+      actors: ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Ellen Page'],
+      notes: 'Great movie',
+    },
+  })
   async create(@Body() createMovieReviewDto: CreateMovieReviewDto) {
     return this.movieReviewsService.create(createMovieReviewDto);
   }
