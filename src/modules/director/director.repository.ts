d7043a9e9a7cd38d5d027860d18e2839aa1fd7b3
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 18:47:07 -0300

    Added method finOneByPersonId in DirectorRepository

--- src/modules/director/director.repository.ts
+++ src/modules/director/director.repository.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Director } from './director.entity';
 
 @Injectable()
 export class DirectorRepository {
-  constructor() {}
+  constructor(
+    @InjectRepository(Director) private repository: Repository<Director>,
+  ) {}
+
+  async findOneByPersonId(personId: number): Promise<Director> {
+    return this.repository.findOne({
+      relations: ['person'],
+      where: { person: { id: personId } },
+    });
+  }
 }
