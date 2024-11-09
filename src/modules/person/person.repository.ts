cccae708db3f487e3d38f4f20388fa2ba850b5ab
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 18:41:24 -0300

    Added method findOneByName in PersonRepository

--- src/modules/person/person.repository.ts
+++ src/modules/person/person.repository.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Person } from './person.entity';
 
 @Injectable()
 export class PersonRepository {
-  constructor() {}
+  constructor(
+    @InjectRepository(Person) private repository: Repository<Person>,
+  ) {}
+
+  async findOneByName(name: string): Promise<Person> {
+    return await this.repository.findOne({ where: { name } });
+  }
 }
