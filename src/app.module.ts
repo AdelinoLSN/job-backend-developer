a335da1ae1e0aa630442674ba0fdd866eedc61cd
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 17:06:26 -0300

    Added entities to TypeOrmModule configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { MovieReview } from './modules/movie-review/movie-review.entity';
+import { Movie } from './modules/movie/movie.entity';
+import { Director } from './modules/director/director.entity';
+import { Actor } from './modules/actor/actor.entity';
+import { Person } from './modules/person/person.entity';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -12,6 +18,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      entities: [MovieReview, Movie, Director, Actor, Person],
+      synchronize: true,
     }),
   ],
   controllers: [],
