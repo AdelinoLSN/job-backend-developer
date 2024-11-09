6f65d8aee7b6bc9a557e4a1e4400fe5bbc998510
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 19:46:43 -0300

    Added method create in MovieReviewRepository

--- src/modules/movie-review/movie-review.repository.ts
+++ src/modules/movie-review/movie-review.repository.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { MovieReview } from './movie-review.entity';
 
 @Injectable()
 export class MovieReviewRepository {
-  constructor() {}
+  constructor(
+    @InjectRepository(MovieReview) private repository: Repository<MovieReview>,
+  ) {}
+
+  async create(movieReview: MovieReview): Promise<MovieReview> {
+    return await this.repository.save(movieReview);
+  }
 }
