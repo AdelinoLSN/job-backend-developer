f299350503b92880b5d0cc98017debcdcbb95b48
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 18:48:01 -0300

    Added method findManyByNameOrCreate in DirectorService

--- src/modules/director/director.service.ts
+++ src/modules/director/director.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Director } from './director.entity';
+import { DirectorRepository } from './director.repository';
+
+import { PersonService } from '../person/person.service';
 
 @Injectable()
 export class DirectorService {
-  constructor() {}
+  constructor(
+    @Inject() private directorRepository: DirectorRepository,
+    @Inject() private personService: PersonService,
+  ) {}
+
+  async findManyByNameOrCreate(names: string[]): Promise<Director[]> {
+    const people = await this.personService.findManyByNameOrCreate(names);
+
+    const directors = await Promise.all(
+      people.map(async (person) => {
+        const directorAlreadyExists =
+          await this.directorRepository.findOneByPersonId(person.id);
+
+        if (directorAlreadyExists) {
+          return directorAlreadyExists;
+        }
+
+        const director = new Director({ person });
+
+        return await this.directorRepository.create(director);
+      }),
+    );
+
+    return directors;
+  }
 }
