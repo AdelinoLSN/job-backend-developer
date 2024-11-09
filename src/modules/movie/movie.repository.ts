7380d92cae4b1bf0316d92629501340e6f357faa
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 19:31:10 -0300

    Added method findOneByTitle in MovieRepository

--- src/modules/movie/movie.repository.ts
+++ src/modules/movie/movie.repository.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Movie } from './movie.entity';
 
 @Injectable()
 export class MovieRepository {
-  constructor() {}
+  constructor(@InjectRepository(Movie) private repository: Repository<Movie>) {}
+
+  async findOneByTitle(title: string): Promise<Movie | null> {
+    return await this.repository.findOne({
+      relations: ['directors', 'actors', 'directors.person', 'actors.person'],
+      where: { title: title },
+    });
+  }
 }
