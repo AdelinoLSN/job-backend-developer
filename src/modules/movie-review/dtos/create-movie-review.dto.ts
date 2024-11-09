9bad5b97cdcbaf94eb50790a35c2cb8bd1e813f5
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 20:24:39 -0300

    Added Swagger documentation to CreateMovieReviewDto

--- src/modules/movie-review/dtos/create-movie-review.dto.ts
+++ src/modules/movie-review/dtos/create-movie-review.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateMovieReviewDto {
   @IsNotEmpty()
   @IsString()
   @MaxLength(255)
+  @ApiProperty({ maxLength: 255 })
   title: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   @MaxLength(65_535)
+  @ApiProperty({ minLength: 3 })
+  @ApiProperty({ maxLength: 65_535 })
   notes: string;
 }
