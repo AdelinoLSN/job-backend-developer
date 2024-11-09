6b857048409367228e2ca5b631f20510ee5956a4
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 18:53:28 -0300

    Added method findManyByNameOrCreate in ActorService

--- src/modules/actor/actor.service.ts
+++ src/modules/actor/actor.service.ts
@@ -1,6 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Actor } from './actor.entity';
+import { ActorRepository } from './actor.repository';
+
+import { PersonService } from '../person/person.service';
 
 @Injectable()
 export class ActorService {
-  constructor() {}
+  constructor(
+    @Inject() private actorRepository: ActorRepository,
+    @Inject() private personService: PersonService,
+  ) {}
+
+  async findManyByNameOrCreate(names: string[]): Promise<Actor[]> {
+    const people = await this.personService.findManyByNameOrCreate(names);
+
+    const actors = await Promise.all(
+      people.map(async (person) => {
+        const actorAlreadyExists = await this.actorRepository.findOneByPersonId(
+          person.id,
+        );
+
+        if (actorAlreadyExists) {
+          return actorAlreadyExists;
+        }
+
+        const actor = new Actor({ person });
+
+        return await this.actorRepository.create(actor);
+      }),
+    );
+
+    return actors;
+  }
 }
