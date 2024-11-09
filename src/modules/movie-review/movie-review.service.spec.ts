e9c8bf457b8502dccfb982212343a0718682d35f
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 20:52:44 -0300

    Added Unit Test to MovieReviewService.create() to ensure the return will be correctly formatted

---
+++ src/modules/movie-review/movie-review.service.spec.ts
@@ -0,0 +1,106 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { MovieReviewService } from './movie-review.service';
+import { MovieReviewRepository } from './movie-review.repository';
+
+import { MovieService } from '../movie/movie.service';
+
+describe(MovieReviewService.name, () => {
+  let movieReviewService: MovieReviewService;
+  let movieReviewRepository: MovieReviewRepository;
+  let movieService: MovieService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        MovieReviewService,
+        {
+          provide: MovieReviewRepository,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+        {
+          provide: MovieService,
+          useValue: {
+            findByTitleOrCreate: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    movieReviewService = module.get<MovieReviewService>(MovieReviewService);
+    movieReviewRepository = module.get<MovieReviewRepository>(
+      MovieReviewRepository,
+    );
+    movieService = module.get<MovieService>(MovieService);
+  });
+
+  describe('create', () => {
+    it('should create a movie review', async () => {
+      const movieReviewDto = {
+        title: 'Inception',
+        notes: 'Great movie',
+      };
+
+      const movie = {
+        id: 1,
+        imdbId: 'tt1375666',
+        title: movieReviewDto.title,
+        releaseDate: new Date('2010-07-16T00:00:00.000Z'),
+        rating: 8.8,
+        directors: [
+          {
+            id: 1,
+            person: {
+              id: 1,
+              name: 'Christopher Nolan',
+            },
+          },
+        ],
+        actors: [
+          {
+            id: 1,
+            person: {
+              id: 1,
+              name: 'Leonardo DiCaprio',
+            },
+          },
+        ],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const movieReview = {
+        id: 1,
+        notes: movieReviewDto.notes,
+        movie,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+      };
+
+      const movieReviewResponse = {
+        movieReviewId: movieReview.id,
+        title: movieReview.movie.title,
+        releaseDate: movieReview.movie.releaseDate,
+        rating: movieReview.movie.rating,
+        directors: movieReview.movie.directors.map(
+          (director) => director.person.name,
+        ),
+        actors: movieReview.movie.actors.map((actor) => actor.person.name),
+        notes: movieReview.notes,
+      };
+
+      jest.spyOn(movieService, 'findByTitleOrCreate').mockResolvedValue(movie);
+
+      jest
+        .spyOn(movieReviewRepository, 'create')
+        .mockResolvedValue(movieReview);
+
+      const result = await movieReviewService.create(movieReviewDto);
+
+      expect(result).toEqual(movieReviewResponse);
+    });
+  });
+});
