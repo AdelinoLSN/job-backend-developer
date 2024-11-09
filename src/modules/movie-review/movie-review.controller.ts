4a4cb4ac6bdaeb9e7923b81df4f175455118d556
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 19:53:05 -0300

    Added endpoint to create MovieReview in MovieReviewController

--- src/modules/movie-review/movie-review.controller.ts
+++ src/modules/movie-review/movie-review.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
+import { CreateMovieReviewDto } from './dtos/create-movie-review.dto';
+import { MovieReviewService } from './movie-review.service';
 
 @Controller('movie-reviews')
 export class MovieReviewController {
-  constructor() {}
+  constructor(private movieReviewsService: MovieReviewService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createMovieReviewDto: CreateMovieReviewDto) {
+    return this.movieReviewsService.create(createMovieReviewDto);
+  }
 }
