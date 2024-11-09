4f3205e5a4c158048771252cd66d310ea63d9d05
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 18:43:01 -0300

    Added method findManyByNameOrCreate in PersonService

--- src/modules/person/person.service.ts
+++ src/modules/person/person.service.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Person } from './person.entity';
+import { PersonRepository } from './person.repository';
 
 @Injectable()
 export class PersonService {
-  constructor() {}
+  constructor(@Inject() private personRepository: PersonRepository) {}
+
+  async findManyByNameOrCreate(names: string[]): Promise<Person[]> {
+    const persons = await Promise.all(
+      names.map(async (name) => {
+        const personAlreadyExists =
+          await this.personRepository.findOneByName(name);
+
+        if (personAlreadyExists) {
+          return personAlreadyExists;
+        }
+
+        const person = new Person({ name });
+
+        return await this.personRepository.create(person);
+      }),
+    );
+
+    return persons;
+  }
 }
