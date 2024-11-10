5fc013a7d2f56adbe58b5a73c65a2bc3d8e49975
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-09 21:01:03 -0300

    Refactored MovieService Unit Test file to an more structured way

--- src/modules/movie/movie.service.spec.ts
+++ src/modules/movie/movie.service.spec.ts
@@ -52,52 +52,54 @@ describe(MovieService.name, () => {
     movieRepository = module.get<MovieRepository>(MovieRepository);
   });
 
-  it('should return movie from database when calling findByTitleOrCreate with a movie that already exists in database', async () => {
-    const title = 'Inception';
-    const movie = {
-      id: 1,
-      imdbId: 'tt1375666',
-      title: 'Inception',
-      releaseDate: new Date('2010-07-16'),
-      rating: 8.8,
-      directors: [],
-      actors: [],
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+  describe('findByTitleOrCreate', () => {
+    it('should return movie from database when the movie exists in database', async () => {
+      const title = 'Inception';
+      const movie = {
+        id: 1,
+        imdbId: 'tt1375666',
+        title: 'Inception',
+        releaseDate: new Date('2010-07-16'),
+        rating: 8.8,
+        directors: [],
+        actors: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
 
-    jest.spyOn(movieRepository, 'findOneByTitle').mockResolvedValue(movie);
+      jest.spyOn(movieRepository, 'findOneByTitle').mockResolvedValue(movie);
 
-    const result = await movieService.findByTitleOrCreate(title);
+      const result = await movieService.findByTitleOrCreate(title);
 
-    expect(result).toEqual(movie);
-  });
+      expect(result).toEqual(movie);
+    });
 
-  it('should throw MultipleMoviesFoundException if multiple movies are found with different titles when calling findByTitleOrCreate without have any movie in database with that name', async () => {
-    const title = 'Inception';
-    const omdbMovies: OmdbMovie[] = [
-      {
-        Title: 'Inception 1',
-        imdbID: 'tt1375666',
-        Year: '2010',
-        Type: 'movie',
-      },
-      {
-        Title: 'Inception 2',
-        imdbID: 'tt0816692',
-        Year: '2014',
-        Type: 'movie',
-      },
-    ];
+    it('should throw MultipleMoviesFoundException if multiple movies are found with different titles and does not exist on database', async () => {
+      const title = 'Inception';
+      const omdbMovies: OmdbMovie[] = [
+        {
+          Title: 'Inception 1',
+          imdbID: 'tt1375666',
+          Year: '2010',
+          Type: 'movie',
+        },
+        {
+          Title: 'Inception 2',
+          imdbID: 'tt0816692',
+          Year: '2014',
+          Type: 'movie',
+        },
+      ];
 
-    jest.spyOn(movieRepository, 'findOneByTitle').mockResolvedValue(null);
+      jest.spyOn(movieRepository, 'findOneByTitle').mockResolvedValue(null);
 
-    jest
-      .spyOn(omdbService, 'searchMoviesByTitle')
-      .mockResolvedValue(omdbMovies);
+      jest
+        .spyOn(omdbService, 'searchMoviesByTitle')
+        .mockResolvedValue(omdbMovies);
 
-    await expect(movieService.findByTitleOrCreate(title)).rejects.toThrow(
-      MultipleMoviesFoundException,
-    );
+      await expect(movieService.findByTitleOrCreate(title)).rejects.toThrow(
+        MultipleMoviesFoundException,
+      );
+    });
   });
 });
