ccfb6d6aae3c759f0bf6d620a02ae9a2e361e8b3
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-11 12:56:28 -0300

    Added integration test for GET /movie-reviews/:id to ensure that an 404 error will be returned when no movie review with given id

--- test/e2e/movie-review.e2e-spec.ts
+++ test/e2e/movie-review.e2e-spec.ts
@@ -348,5 +348,19 @@ describe(`${MovieReview.name} (e2e)`, () => {
           });
         });
     });
+
+    it('should return a 404 error when the movie review does not exist', () => {
+      const movieReviewId = faker.number.int();
+
+      return request(app.getHttpServer())
+        .get(`/movie-reviews/${movieReviewId}`)
+        .expect(HttpStatus.NOT_FOUND)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            statusCode: HttpStatus.NOT_FOUND,
+            message: `Movie review with id "${movieReviewId}" not found`,
+          });
+        });
+    });
   });
 });
