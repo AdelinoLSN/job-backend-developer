c16f9f04d5687e05bb39bf41ad6829fe09566a23
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-19 09:20:04 -0300

    Refactored: ActorRepository to use Dependency Inversion principle

--- src/modules/actor/actor.module.ts
+++ src/modules/actor/actor.module.ts
@@ -4,13 +4,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Actor } from './actor.entity';
 import { ActorService } from './actor.service';
 import { ActorRepository } from './actor.repository';
+import { TypeOrmActorRepository } from './typeorm-actor.repository';
 
 import { PersonModule } from '../person/person.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Actor]), PersonModule],
   controllers: [],
-  providers: [ActorService, ActorRepository],
+  providers: [
+    ActorService,
+    {
+      provide: ActorRepository,
+      useClass: TypeOrmActorRepository,
+    },
+  ],
   exports: [ActorService],
 })
 export class ActorModule {}
