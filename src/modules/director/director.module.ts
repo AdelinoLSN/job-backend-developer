865d7fd05820a669f8cce62ab570d72dff16ebec
Adelino Lourenço <adelinolsn@gmail.com>
2024-11-19 09:22:08 -0300

    Refactored: DirectorRepository to use Dependency Inversion principle

--- src/modules/director/director.module.ts
+++ src/modules/director/director.module.ts
@@ -4,13 +4,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Director } from './director.entity';
 import { DirectorService } from './director.service';
 import { DirectorRepository } from './director.repository';
+import { TypeOrmDirectorRepository } from './typeorm-director.repository';
 
 import { PersonModule } from '../person/person.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Director]), PersonModule],
   controllers: [],
-  providers: [DirectorService, DirectorRepository],
+  providers: [
+    DirectorService,
+    {
+      provide: DirectorRepository,
+      useClass: TypeOrmDirectorRepository,
+    },
+  ],
   exports: [DirectorService],
 })
 export class DirectorModule {}
